58a1a769fd31aad7633ec35f696b996609c4c02b
niadegebairen <33858639+niadegebairen@users.noreply.github.com>
2018-08-27 18:28:20 +0800

    Update Double_coulor_ball.h

--- Double_coulor_ball.h
+++ Double_coulor_ball.h
@@ -1,13 +1,20 @@
-#include<iostream>
+#ifndef DOUBLE_H
+#define DOUBLE_H
 #include<array>
-#include<vector>
-
-class Double_coulor_ball{
-public:
-  Double_coulor_ball();
-  std::array<int,7> result() {return doubleCoulorBallResult;}
-  int appearCount() {return appearCount;}
-private:
-  std::array<int,7> doubleCoulourBallResult;
-  int appearCount;
+class Double_color_ball {
+	public:
+	Double_color_ball();
+	Double_color_ball(unsigned a1, unsigned a2, unsigned a3, unsigned a4, unsigned a5, unsigned a6, unsigned b) :
+		doubleColorBallResult({ a1, a2, a3, a4, a5, a6, b} ) {}
+	Double_color_ball(std::array<unsigned,7> ua,size_t count):
+		doubleColorBallResult(ua),appearanceCount(count) {}
+	std::array<unsigned, 7> Result() const { return doubleColorBallResult; }
+	void AppearOnceAgain()  { ++appearanceCount; }
+	size_t AppearanceCount() const { return appearanceCount; }
+	private:
+	std::array<unsigned, 7> doubleColorBallResult;
+	size_t appearanceCount{ 0 };
 };
+
+bool unfrequent(const Double_color_ball&, const Double_color_ball&);
+#endif
